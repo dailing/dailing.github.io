5132a3bd350ffb1b579ed8318aa477f3c27056f1
dailing <qzyz_dailing@163.com>
2019-06-19 19:31:21 +0800

    adding code to transform the fucking thing

--- presentation.my/index.js
+++ presentation.my/index.js
@@ -11,14 +11,15 @@ Vue.component('slide', {
     props: ['title'],
     methods: {
     },
-    mounted : function(){
+    mounted(){
         console.log(this);
-        me_index = this.$parent.register_slide();
+        me_index = this.$parent.register_slide(this);
         this.slide_index = me_index;
     },
     template: 
 `
-<div class="border border-warning slide" v-if="$parent.current_slide==slide_index">
+<div class="border border-warning slide " 
+        v-bind:class="{ active: $parent.current_slide == slide_index}">
     <slot name='header'>
         <div class="slide_head">
             <h1>{{ title }}</h1>
@@ -42,22 +43,57 @@ var app = new Vue({
     data: {
         message: 'Hello Vue!',
         slide_count : 0,
+        slides : [],
         current_slide : null,
         current_url : null,
+        w_height:0,
+        w_width:0,
+        transform_tleft:0,
+        transform_ttop:0,
+        transform_scale:1
     },
     methods:{
-        register_slide(){
+        register_slide(child){
             this.slide_count += 1;
+            this.slides.push(child);
             return this.slide_count - 1;
         },
         get_url_for_page(i){
             return this.current_url + "#" + i;
         },
         goto(page){
+            prev_page = this.current_slide;
             page = Math.min(page, this.slide_count-1);
             page = Math.max(page, 0);
             this.current_slide = page;
             window.history.replaceState(null, null, this.get_url_for_page(this.current_slide));
+            if(prev_page != page){
+                // animation here
+                box = this.slides[page].$el.getBoundingClientRect()
+                ratio = window.innerWidth / box.width;
+                ratio = Math.min(window.innerHeight / box.height);
+                this.transform_scale *= ratio;
+                this.transform_tleft -= box.left;
+                this.transform_ttop -= box.top
+
+                transform = 'translate('+ this.transform_tleft +
+                    'px,' + this.transform_ttop + 'px)' + ' scale(' + 
+                    this.transform_scale + ')';
+                console.log(transform);
+                origin = 'top left';
+                // console.log(this.slides[page].$el.style)
+				document.body.style.transformOrigin = origin;
+				document.body.style.OTransformOrigin = origin;
+				document.body.style.msTransformOrigin = origin;
+				document.body.style.MozTransformOrigin = origin;
+				document.body.style.WebkitTransformOrigin = origin;
+
+				document.body.style.transform = transform;
+				document.body.style.OTransform = transform;
+				document.body.style.msTransform = transform;
+				document.body.style.MozTransform = transform;
+				document.body.style.WebkitTransform = transform;
+            }
         },
         prev(){
             this.goto(this.current_slide - 1);
@@ -80,6 +116,8 @@ var app = new Vue({
         }else{
             this.goto(0);
         }
+        this.w_height = window.innerHeight;
+        this.w_width = window.innerWidth;
         console.log(this);
         console.log('get current url ' + this.current_url);
     }
